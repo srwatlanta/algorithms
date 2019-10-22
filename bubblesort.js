f20ea7d6daf6f5fb3f7437a45a70be459a2b037e
srwatlanta <shawnrwheeler@gmail.com>
2019-10-22 11:16:30 -0400

    bubblesort

---
+++ bubblesort.js
@@ -0,0 +1,15 @@
+const bubbleSort = arr => {
+    let noSwaps
+    for(let i = arr.length; i > 0; i--){
+      noSwaps = true
+      for(let j = 0; j < i -1; j++){
+        let k = j + 1
+        if(arr[j] > arr[k]){
+          [arr[j], arr[k]] = [arr[k], arr[j]]
+          noSwaps = false
+        }
+      }
+      if(noSwaps === true) break
+    }
+    return arr
+  }
