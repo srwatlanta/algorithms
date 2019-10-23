c65ad2d59a6274ff229d48b95b05b3dbb16da88e
srwatlanta <shawnrwheeler@gmail.com>
2019-10-23 02:10:58 -0400

    leftrotate

---
+++ leftrotate.js
@@ -0,0 +1,7 @@
+function rotLeft(a, d) {
+    let shift = (a.length -d) % a.length
+    let p1 = a.slice(-shift)
+    let p2 = a.slice(0, -shift)
+    let newArr = p1.concat(p2)
+    return newArr
+  }
