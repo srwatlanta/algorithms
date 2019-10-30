515a6e94e907992d722d3e4339b9de4d35179132
srwatlanta <shawnrwheeler@gmail.com>
2019-10-30 13:52:31 -0500

    repeated string

--- exercises/leftrotate.js
+++ exercises/leftrotate.js
@@ -1,5 +1,5 @@
 function rotLeft(a, d) {
-    let shift = (a.length -d) % a.length
+    let shift = (a.length - d) % a.length
     let p1 = a.slice(-shift)
     let p2 = a.slice(0, -shift)
     let newArr = p1.concat(p2)
--- exercises/repeatedstring.js
+++ exercises/repeatedstring.js
@@ -1,10 +1,21 @@
 function repeatedString(s, n) {
-    if(s.length === 1 && s === 'a') return n
-    let aCount = 0
-    for(let i = 0; i < n; i ++){
-      if(s[i] === 'a' || s[i % s.length] === 'a'){
-        aCount += 1
+  if(s.length === 1 && s === 'a') return n
+  let aCount = 0
+  let times = Math.floor(n / s.length)
+  let remainder = n % (s.length * times)
+  let run = Math.min(n, s.length)
+  for(let i = 0; i < run; i++){
+    if(s[i] === 'a') aCount += 1
+  }
+  if(times === 0)return aCount
+  else{
+    if(remainder === 0)return (aCount * times)
+    else{
+      aCount = aCount * times
+      for(let i = 0; i < remainder; i++){
+        if (s[i] === 'a') aCount += 1
       }
+      return aCount
     }
-    return aCount
+  }
 }
