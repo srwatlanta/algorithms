a5beb5f2b4d40f201825e292db31936ed1a0eec1
srwatlanta <shawnrwheeler@gmail.com>
2019-11-04 16:07:47 -0600

    max depth

--- datastructures/bst.js
+++ datastructures/bst.js
@@ -111,5 +111,27 @@ class BST{
     traverse(current)
     return tree[k-1]
   }
+
+  maxDepth(root) {
+    if(root === null) return 0
+   let maxCount = 1
+   let count = 1
+   function traverse(node){
+     maxCount = Math.max(count, maxCount)
+     if(node.left !== null){
+       count++
+       traverse(node.left)
+       count--
+     }
+     if(node.right !== null){
+       count ++
+       traverse(node.right)
+       count --
+     }
+   }
+   traverse(root)
+   return maxCount
+    
+};
   
 }
