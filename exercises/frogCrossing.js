db3dd7bbebc515b5ced247382daccb2ee209f0ad
srwatlanta <shawnrwheeler@gmail.com>
2019-12-14 17:43:21 -0600

    frog crossing

---
+++ exercises/frogCrossing.js
@@ -0,0 +1,26 @@
+function frogCrossing(arr, x){
+    let tracker = {}
+    let allTrue = false
+    for(let i = x; i > 0; i--){
+      tracker[i] = 1
+    }
+    let found = 0
+    for(let j = 0; j < arr.length; j++) {
+      if(tracker[arr[j]]) {
+        tracker[arr[j]] = true
+      }
+      if(trackerChecker(tracker) === true){
+        found = j
+        break
+      }
+    }
+    return found
+  }
+  function trackerChecker(tracker){
+    for(const k in tracker){
+      if(tracker[k] === 1){
+        return false
+      }
+    }
+    return true
+  }
